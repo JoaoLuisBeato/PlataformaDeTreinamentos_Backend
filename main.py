b7b721e8882cecb37a19b8936b0888fed53b69e4
Beato <joaoluisbeato@outlook.com>
2023-05-15 19:09:16 -0300

    Update nos comentários da API

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ app = Flask(__name__)
 
 @app.route('/cadastro', methods=['POST'])
 def cadastro():
+
+    # Recebendo email e senha do usuário
     email = request.form['email']
     password = request.form['password']
 
+    # Inicialiazação dos parâmetros para o banco de dados
     mycursor = db.cursor()
-
+    # Insere um usuário novo no banco de dados
     sql_command = "INSERT INTO usuarios (email, senha, tipo_usuario) VALUES (%s, %s, %s)"
     values = (email, password, 'Aluno')
-
     mycursor.execute(sql_command, values)
+    # Salvas as alterações feitas no banco de dados
     db.commit()
 
     print(email)
     print(password)
+    # Retorna um valor para o frontend
     return 'Cadastro Realizado'
 
 
@@ -34,19 +38,23 @@ def cadastro():
 @app.route('/login', methods=['POST'])
 def login():
 
+    # Recebendo email e senha do usuário
     email = request.form['email']
     password = request.form['password']
     
+    # Inicialiazação dos parâmetros para o banco de dados
     mycursor = db.cursor()
 
+    # Procura no banco de dados um usuário com o email que foi passado
     sql_command = "SELECT email, senha FROM usuarios Where email = %s"
     value = (email,)
     mycursor.execute(sql_command, value)
     email_res = mycursor.fetchone()
-    if email_res is not None:
-        senha_encontrada = email_res[1]
-        if senha_encontrada == password:
 
+    # Verefica se o retorno não foi nulo e se a senha inserida é igual a cadastrada
+    if email_res is not None:
+        senha_encontrada = email_res[1] #posicao da coluna da tabela do banco de dado
+        if senha_encontrada == password:
             print("Logado com sucesso")
             return jsonify({'acesso': 'true'})
         else:
@@ -66,11 +74,14 @@ def teste_tdd():
     #TESTE AUTOMATIZADO
     #Verefica a existencia de um login
 
+    # Recebendo email e senha do usuário
     email = request.json.get('email')
     password = request.json.get('password')
 
+    # Inicialiazação dos parâmetros para o banco de dados
     mycursor = db.cursor()
 
+    # Procura no banco de dados um usuário com o email que foi passado
     sql_command = "SELECT email, senha FROM usuarios Where email = %s"
     value = (email,)
     mycursor.execute(sql_command, value)
