d945bf002f02b412b44cf3f729c4c0d8d19a0bff
Mateus José <mjwf@uol.com.br>
2023-05-25 12:56:07 -0300

    Deletes de treinamento e vagas funcionando

--- main.py
+++ main.py
@@ -240,10 +240,12 @@ def sair_treinamento():
 
     sql_command = "DELETE FROM treinamento_alunos WHERE email = %s"
     value = (email,)
+    mycursor.execute(sql_command, value)
     db.commit()
 
     sql_command = "UPDATE treinamentos WHERE Codigo_curso = %s SET qntd_atual = qntd_atual -1"
     value = (codigo_treinamento,)
+    mycursor.execute(sql_command, value)
     db.commit()
 
     return jsonify({'status_delete' : 'Deletado com sucesso!'})
@@ -342,12 +344,13 @@ def listar_vagas():
 
     for i in range(tamanho):
         vaga = {
-        'Titulo da vaga': vagas_emprego[i][0],
-        'Empresa': vagas_emprego[i][1],
-        'Descricao':vagas_emprego[i][2],
-        'Pré Requisito': vagas_emprego[i][3],
-        'Salário mínimo': vagas_emprego[i][4],
-        'Salário máximo': vagas_emprego[i][5],
+        'id': vagas_emprego[i][0],
+        'Titulo da vaga': vagas_emprego[i][1],
+        'Empresa': vagas_emprego[i][2],
+        'Descricao':vagas_emprego[i][3],
+        'Pré Requisito': vagas_emprego[i][4],
+        'Salário mínimo': vagas_emprego[i][5],
+        'Salário máximo': vagas_emprego[i][6],
         }
 
         listaVagas.append(vaga)
@@ -441,9 +444,20 @@ def Update_treinamentos():
 @app.route('/Delete_treinamentos', methods=['POST'])
 def Delete_treinamentos():
     mycursor = db.cursor()
-    codigo_curso = request.form('codigo_curso')
-    sql_command = "DELETE * FROM treinamentos WHERE Codigo_curso = %s"
+    codigo_curso = request.form['codigo_curso']
+    print(codigo_curso)
+    sql_command = "DELETE FROM treinamentos WHERE Codigo_curso = %s"
     values = (codigo_curso,)
+    mycursor.execute(sql_command, values)
+    db.commit()
+    return 'penes'
+
+@app.route('/Delete_vagas', methods=['POST'])
+def Delete_vagas():
+    mycursor = db.cursor()
+    codigo_vagas = request.form['codigo_vaga']
+    sql_command = "DELETE FROM vaga_emprego WHERE id = %s"
+    values = (codigo_vagas,)
     mycursor.execute(sql_command, values)
     db.commit()
     return 'penes'
